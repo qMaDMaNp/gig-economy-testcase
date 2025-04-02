3a86c230da7bf9e190da65b5769a8ebf80367b30
qMaDMaNp <medifman@gmail.com>
2025-04-02 12:36:16 -0400

    Add routes and controller boilerplate

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,35 @@
 <?php
 
-use App\Http\Middleware\CorsMiddleware;
 use Illuminate\Support\Facades\Route;
 
+// Middlewares
+use App\Http\Middleware\CorsMiddleware;
+
+// Controllers
+use App\Http\Controllers\BranchController;
+use App\Http\Controllers\ManagerController;
+use App\Http\Controllers\VehicleController;
+
 Route::middleware([CorsMiddleware::class])->group(function () {
-    Route::get('/', function () {
-        return 'oyyy leleka';
+    Route::prefix('branches')->group(function () {
+        Route::get('/', [BranchController::class, 'index']);
+        Route::get('/{id}', [BranchController::class, 'show']);
+
+        Route::post('/', [BranchController::class, 'store']);
+    });
+
+    Route::prefix('managers')->group(function () {
+        Route::get('/', [ManagerController::class, 'index']);
+        Route::get('/{id}', [ManagerController::class, 'show']);
+        
+
+        Route::post('/', [ManagerController::class, 'store']);
+    });
+
+    Route::prefix('vehicles')->group(function () {
+        Route::get('/', [VehicleController::class, 'index']);
+        Route::get('/{id}', [VehicleController::class, 'show']);
+
+        Route::post('/', [VehicleController::class, 'store']);
     });
 });
