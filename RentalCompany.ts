d859ea1920acc0af9a07aa761d8885410411bc8d
qMaDMaNp <medifman@gmail.com>
2025-04-02 09:43:17 -0400

    Add task description and rental company class

---
+++ RentalCompany.ts
@@ -0,0 +1,19 @@
+/*
+Task
+
+- Mamy wieloodziałową wypożyczalnię samochodów. 
+- W każdym oddziale może być jedna lub kilka osób odpowiedzialnych za zarządzanie pojazdami. 
+- Pojazdy są różnych typów, mogą to być samochody, rowery, skutery, hulajnogi i w przyszłości inne.
+*/
+
+
+/*
+Entites
+
+Branches
+Managers
+Vehicles
+*/
+
+
+
